d6b395e821b06dae9ee7cbb7462efb32cb133aa6
varung97 <varung97@gmail.com>
2017-05-07 07:42:30 +0800

    Dynamic resizing board

--- static/js/motion.js
+++ static/js/motion.js
@@ -9,3 +9,28 @@ window.addEventListener('deviceorientation', function(event) {
         // dataContainerOrientation.innerHTML = 'No device orientation data'
     }
 }, false);
+
+window.addEventListener('resize', setBoardDims, true);
+
+function init() {
+    setBoardDims();
+    showBoard();
+}
+
+function setBoardDims() {
+    var board = document.getElementById('board');
+    var square_side = null;
+    if (window.innerHeight < window.innerWidth) {
+        square_side = window.innerHeight - 20;
+    } else {
+        square_side = window.innerWidth - 20;
+    }
+
+    board.style.height = square_side + 'px';
+    board.style.width = square_side + 'px';
+}
+
+function showBoard() {
+    var board = document.getElementById('board');
+    board.style.display = 'block';
+}
